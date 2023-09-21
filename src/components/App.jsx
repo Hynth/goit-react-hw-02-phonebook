1dffd1f38e6e4413a7bf693d996b8f34f64b4b4d
Hynth <cdar93@gmail.com>
2023-09-21 08:40:49 -0500

    task phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+import Phonebook from "./Phonebook";
+import SearchContacts from "./SearchContacts";
+import ShowContacts from "./ShowContacts";
+
 export const App = () => {
+  const [state, setState] = useState({
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
+    ],
+    filter: '',
+    name: '',
+    number: ''
+  });
+
+  const [showContact, setShowContact] = useState(state.contacts)
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Phonebook state={state} setState={setState} setShowContact={setShowContact}/>      
+      <SearchContacts state={state} setState={setState} setShowContact={setShowContact}/>
+      <ShowContacts contacts={showContact} state={state} setState={setState} setShowContact={setShowContact}/>
     </div>
   );
 };
